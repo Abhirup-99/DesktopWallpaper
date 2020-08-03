4393001123b59b4b2b09d3cf06dc5542072058b8
Abhirup-2019 <pabhirup@tomilkieway.com>
2020-08-03 06:02:09 +0530

    fallback

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from scraper import scrape
 from setWallpaper import setWallpaper
 import argparse
 from random import randint
+import urllib.request
+
+
+def connect():
+    try:
+        urllib.request.urlopen('https://www.google.com/') #Python 3.x
+        return True
+    except:
+        return False
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=\
@@ -9,9 +18,13 @@ if __name__ == "__main__":
     parser.add_argument("--term",type=str,nargs="+")
     parser.set_defaults(term=[])
     args =  parser.parse_args()
-    if(len(args.term)==0):
-        scrape("")
-    else:
-        randomGuess = randint(0,len(args.term)-1)
-        scrape(args.term[randomGuess])
-    setWallpaper()
+
+    #checks if its connected to the internet
+    isInternetConnected = connect()
+    if isInternetConnected:
+        if(len(args.term)==0):
+            scrape("")
+        else:
+            randomGuess = randint(0,len(args.term)-1)
+            scrape(args.term[randomGuess])
+        setWallpaper()
