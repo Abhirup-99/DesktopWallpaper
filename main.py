8619c1704b64563f99c1e764a79d68e02ba73699
Abhirup Pal <abhiruppalmethodist@gmail.com>
2021-05-30 22:35:18 +0530

    format: Add ci.

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ import urllib.request
 
 def connect():
     try:
-        urllib.request.urlopen('https://www.google.com/') #Python 3.x
+        urllib.request.urlopen("https://www.google.com/")  # Python 3.x
         return True
     except:
         return False
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=\
-        "Helper methods")
-    parser.add_argument("--term",type=str,nargs="+")
-    parser.set_defaults(term=[])
-    args =  parser.parse_args()
 
-    #checks if its connected to the internet
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Helper methods")
+    parser.add_argument("--term", type=str, nargs="+")
+    parser.set_defaults(term=[])
+    args = parser.parse_args()
+
+    # checks if its connected to the internet
     print("starting")
     isInternetConnected = connect()
     if isInternetConnected:
-        if(len(args.term)==0):
+        if len(args.term) == 0:
             print("starting scraping")
             scrape("")
         else:
-            randomGuess = randint(0,len(args.term)-1)
+            randomGuess = randint(0, len(args.term) - 1)
             scrape(args.term[randomGuess])
         print("setting wallpaper")
--- scraper.py
+++ scraper.py
@@ -3,35 +3,42 @@ import shutil
 import requests
 import time
 import os
-from random import  randint
+from random import randint
 
-download_direct = os.path.join(os.path.dirname(__file__), 'images')
+download_direct = os.path.join(os.path.dirname(__file__), "images")
+
 
 def scrape(term=""):
     try:
-        #checks if folder is already there
+        # checks if folder is already there
         if not os.path.exists(download_direct):
             os.makedirs(download_direct)
         else:
             shutil.rmtree(download_direct)
             os.makedirs(download_direct)
 
-        #assumes there are 10 pages for a search result
-        page = randint(1,10)
-        if (term != ""):
-            url = "https://www.wallpaperflare.com/search?wallpaper={}&page={}".format(term, page)
+        # assumes there are 10 pages for a search result
+        page = randint(1, 10)
+        if term != "":
+            url = "https://www.wallpaperflare.com/search?wallpaper={}&page={}".format(
+                term, page
+            )
         else:
-            url = "https://www.wallpaperflare.com/index.php?c=main&m=portal_loadmore&page={}".format(page)
-        
+            url = "https://www.wallpaperflare.com/index.php?c=main&m=portal_loadmore&page={}".format(
+                page
+            )
+
         page_soup = soup(requests.get(url).content, "html5lib")
 
-        imgs = page_soup.findAll("li", {"itemprop":"associatedMedia"})
+        imgs = page_soup.findAll("li", {"itemprop": "associatedMedia"})
 
-
-        #gets a random image from the page
-        img_random_number = randint(1,len(imgs)-1)
-        img_page = imgs[img_random_number].find("a", {"itemprop":"url"})["href"] + "/download/"
-        img_url=get_url(img_page)
+        # gets a random image from the page
+        img_random_number = randint(1, len(imgs) - 1)
+        img_page = (
+            imgs[img_random_number].find("a", {"itemprop": "url"})["href"]
+            + "/download/"
+        )
+        img_url = get_url(img_page)
         download_file(img_url)
         return "success"
     except Exception:
@@ -40,10 +47,11 @@ def scrape(term=""):
 
 def get_url(img_page):
     page_soup = soup(requests.get(img_page).content, "html.parser")
-    return page_soup.find("img", {"id":"show_img"})["src"]
+    return page_soup.find("img", {"id": "show_img"})["src"]
+
 
 def download_file(url):
-    local_filename = url.split('/')[-1]
+    local_filename = url.split("/")[-1]
     with requests.get(url, stream=True) as r:
         with open(os.path.join(download_direct, local_filename), "wb") as f:
             shutil.copyfileobj(r.raw, f)
